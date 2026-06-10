ab2893614738e3a390b6ccc50719e47d8e5c8d50
yilerler <yilerler@gmail.com>
2026-06-10 18:00:41 +0800

    Delete ot_defense_layer/.modules.order.cmd

--- ot_defense_layer/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/yilerler/mcs-edge-controller/ot_defense_layer/modules.order := {   echo /home/yilerler/mcs-edge-controller/ot_defense_layer/src/mock_elc_core.o; :; } > /home/yilerler/mcs-edge-controller/ot_defense_layer/modules.order
